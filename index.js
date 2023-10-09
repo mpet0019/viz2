eaffe795db7a421a3336a8419bd66ea3ebee9238
mpet0019 <144070387+mpet0019@users.noreply.github.com>
2023-10-10 00:06:30 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var vg_1 = "top_langs.json";
+vegaEmbed("#top_langs", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "language_scatter.json";
+vegaEmbed("#language_scatter", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "aus_pie.json";
+vegaEmbed("#aus_pie", vg_3).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "world_pie.json";
+vegaEmbed("#world_pie", vg_4).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "map.vl.json";
+vegaEmbed("#map", vg_5).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
